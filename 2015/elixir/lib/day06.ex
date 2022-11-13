41dac294354d5455ae5e8a472e0b5be970594a3d
Filip Bontin <filip.bontin@devoteam.com>
2022-11-13 17:31:48 +0100

    Day 6, 2015 (part 1)

---
+++ 2015/elixir/lib/day06.ex
@@ -0,0 +1,51 @@
+defmodule Day6 do
+  def read_input() do
+    File.read!('data/06.txt') |> String.split("\n")
+  end
+
+  def parse_point({x, y}) do
+    {String.to_integer(x), String.to_integer(y)}
+  end
+
+  def parse_row(str) do
+    ~r/(on|off|toggle)\s(\d+),(\d+)\D+(\d+),(\d+)/
+    |> Regex.scan(str)
+    |> Enum.map(fn [_, i, x1, y1, x2, y2] -> [i, {x1, y1}, {x2, y2}] end)
+    |> Enum.map(fn [i, p1, p2] -> [i, parse_point(p1), parse_point(p2)] end)
+  end
+
+  def get_points({x1, y1}, {x2, y2}) do
+    for x <- x1..x2, y <- y1..y2, do: {x, y}
+  end
+
+  def toggle_light(on?) do
+    case on? do
+      1 -> 0
+      0 -> 1
+    end
+  end
+
+  def run(["on", p1, p2], lights) do
+    get_points(p1, p2) |> Enum.into(lights, fn p -> {p, 1} end)
+  end
+
+  def run(["off", p1, p2], lights) do
+    get_points(p1, p2) |> Enum.into(lights, fn p -> {p, 0} end)
+  end
+
+  def run(["toggle", p1, p2], lights) do
+    get_points(p1, p2)
+    |> Enum.into(%{}, fn p -> {p, 1} end)
+    |> Map.merge(lights, fn _k, _l1, l2 -> toggle_light(l2) end)
+  end
+
+  def part1() do
+    read_input()
+    |> Enum.map(&parse_row/1)
+    |> Enum.map(&List.flatten/1)
+    |> Enum.filter(&(!Enum.empty?(&1)))
+    |> Enum.reduce(%{}, &run/2)
+    |> Map.values()
+    |> Enum.sum()
+  end
+end
