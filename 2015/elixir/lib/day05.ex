0ae574d555034fc8215383e517aff0d027ca4825
Filip Bontin <filip.bontin@devoteam.com>
2022-11-13 15:59:19 +0100

    Day5, 2015 (part 2)

--- 2015/elixir/lib/day05.ex
+++ 2015/elixir/lib/day05.ex
@@ -28,4 +28,25 @@ defmodule Day5 do
   def part1() do
     read_input() |> Enum.map(&is_nice/1) |> Enum.count(& &1)
   end
+
+  def has_reoccuring_part(str) do
+    str
+    |> String.graphemes()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [c1, c2] -> c1 <> c2 end)
+    |> Enum.map(&Regex.scan(~r/#{&1}/, str))
+    |> Enum.any?(&(Enum.count(&1) >= 2))
+  end
+
+  def has_repeat_letter(str) do
+    Regex.match?(~r/(\w)\w\1/, str)
+  end
+
+  def is_nice_2(str) do
+    has_reoccuring_part(str) && has_repeat_letter(str)
+  end
+
+  def part2() do
+    read_input() |> Enum.map(&is_nice_2/1) |> Enum.count(& &1)
+  end
 end
