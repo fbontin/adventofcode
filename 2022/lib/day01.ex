48c7fcbe8a61f3e71f6f3d1ef3b1f981f79c96f6
Filip Bontin <filip.bontin@devoteam.com>
2022-12-01 08:48:23 +0100

    Day 1, 2022

--- 2022/lib/day01.ex
+++ 2022/lib/day01.ex
@@ -1,9 +1,18 @@
 defmodule Day01 do
   def read_input() do
     File.read!('data/01.txt')
+    |> String.trim()
+    |> String.split("\n\n")
+    |> Enum.map(&String.split(&1, "\n"))
   end
 
+  def sum_elf(l), do: l |> Enum.map(&String.to_integer/1) |> Enum.sum()
+
   def part1() do
-    read_input()
+    read_input() |> Enum.map(&sum_elf/1) |> Enum.max()
+  end
+
+  def part2() do
+    read_input() |> Enum.map(&sum_elf/1) |> Enum.sort(:desc) |> Enum.take(3) |> Enum.sum()
   end
 end
