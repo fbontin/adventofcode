26241505e1b9a3472bd85ac3d558776c5e5c6782
Filip Bontin <fbontin@users.noreply.github.com>
2023-11-24 08:54:04 +0100

    Day 3, part 2, 2015 (rust)

--- 2015/rust/day03/src/main.rs
+++ 2015/rust/day03/src/main.rs
@@ -6,31 +6,23 @@ struct Point {
     y: i32,
 }
 
+fn walk(d: char, p: Point) -> Point {
+    match d {
+        '<' => Point { x: p.x - 1, ..p },
+        '^' => Point { y: p.y + 1, ..p },
+        '>' => Point { x: p.x + 1, ..p },
+        'v' => Point { y: p.y - 1, ..p },
+        a => panic!("Not implemented for {a:?}"),
+    }
+}
+
 fn part_1() {
     let mut current = Point { x: 0, y: 0 };
     let mut houses = HashSet::new();
     houses.insert(Point { x: 0, y: 0 });
 
     for d in include_str!("../data.txt").chars() {
-        current = match d {
-            '<' => Point {
-                x: current.x - 1,
-                ..current
-            },
-            '^' => Point {
-                y: current.y + 1,
-                ..current
-            },
-            '>' => Point {
-                x: current.x + 1,
-                ..current
-            },
-            'v' => Point {
-                y: current.y - 1,
-                ..current
-            },
-            a => panic!("Not implemented for {a:?}"),
-        };
+        current = walk(d, current);
         houses.insert(current);
     }
     let size = houses.len();
@@ -38,7 +30,28 @@ fn part_1() {
     println!("Part 1: {size:?}");
 }
 
+fn part_2() {
+    let mut santa = Point { x: 0, y: 0 };
+    let mut robo = Point { x: 0, y: 0 };
+    let mut houses = HashSet::new();
+    houses.insert(Point { x: 0, y: 0 });
+
+    for (i, d) in include_str!("../data.txt").chars().enumerate() {
+        if i % 2 == 0 {
+            santa = walk(d, santa);
+            houses.insert(santa);
+        } else {
+            robo = walk(d, robo);
+            houses.insert(robo);
+        }
+    }
+    let size = houses.len();
+
+    println!("Part 2: {size:?}");
+}
+
 fn main() {
     part_1();
+    part_2();
 }
 
