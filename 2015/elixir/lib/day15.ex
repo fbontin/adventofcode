8b444fad218ab149595ce39292a8368523734422
Filip Bontin <fbontin@users.noreply.github.com>
2023-12-02 18:22:33 +0100

    Day 15, 2015 (reworked and solved part 2)

--- 2015/elixir/lib/day15.ex
+++ 2015/elixir/lib/day15.ex
@@ -1,44 +1,76 @@
 defmodule Day15 do
   def read_input() do
-    File.read!('data/15.txt') |> String.trim() |> String.split("\n")
+    # Manually parsed :-)
+    [
+      %{name: :spr, capacity: 2, durability: 0, flavor: -2, texture: 0, calories: 3},
+      %{name: :but, capacity: 0, durability: 5, flavor: -3, texture: 0, calories: 3},
+      %{name: :cho, capacity: 0, durability: 0, flavor: 5, texture: -1, calories: 8},
+      %{name: :can, capacity: 0, durability: -1, flavor: 0, texture: 5, calories: 8}
+    ]
   end
 
-  def parse_part(str) do
-    String.split(str, " ") |> List.last() |> String.to_integer()
-  end
-
-  def parse_row(str) do
-    str |> String.split(",") |> Enum.map(&parse_part/1)
-  end
-
-  def get_all_recipes() do
-    for x <- 1..100 do
-      for y <- 1..(100 - x) do
-        for z <- 1..(100 - x - y) do
-          for w <- 1..(100 - x - y - z) do
-            [{x, y, z, w}]
-          end
-        end
-      end
+  def get_quadruples do
+    for a <- 0..100,
+        b <- 0..(100 - a),
+        c <- 0..(100 - a - b),
+        d <- 0..(100 - a - b - c),
+        a + b + c + d == 100 do
+      {a, b, c, d}
     end
-    |> List.flatten()
   end
 
-  def calc({n1, n2, n3, n4}, [i1, i2, i3, i4]) do
-    calc_ingr = fn i, n -> Enum.map(i, &(&1 * n)) end
+  def quadruple_to_recipe({spr, but, cho, can}), do: %{spr: spr, but: but, cho: cho, can: can}
 
-    [_calories | rest] =
-      [calc_ingr.(i1, n1), calc_ingr.(i2, n2), calc_ingr.(i3, n3), calc_ingr.(i4, n4)]
-      |> List.zip()
-      |> Enum.map(fn {x, y, z, w} -> max(x + y + z + w, 0) end)
-      |> Enum.reverse()
+  def sum(a, i) do
+    %{
+      name: i.name,
+      capacity: a * i.capacity,
+      durability: a * i.durability,
+      flavor: a * i.flavor,
+      texture: a * i.texture,
+      calories: a * i.calories
+    }
+  end
 
-    rest |> Enum.reduce(&*/2)
+  def sum_ingrs(ingrs, property) do
+    ingrs |> Enum.map(fn i -> i[property] end) |> Enum.sum() |> max(0)
+  end
+
+  def calc(recipe, ingredients) do
+    all =
+      [:spr, :but, :cho, :can]
+      |> Enum.map(fn name ->
+        sum(recipe[name], ingredients |> Enum.find(&(&1.name == name)))
+      end)
+
+    res = %{
+      recipe: recipe,
+      capacity: all |> sum_ingrs(:capacity),
+      durability: all |> sum_ingrs(:durability),
+      flavor: all |> sum_ingrs(:flavor),
+      texture: all |> sum_ingrs(:texture),
+      calories: all |> sum_ingrs(:calories)
+    }
+
+    {res, [res.capacity, res.durability, res.flavor, res.texture] |> Enum.product()}
   end
 
   def part1() do
-    ingredients = read_input() |> Enum.map(&parse_row/1)
+    ingredients = read_input()
 
-    get_all_recipes() |> Enum.map(&calc(&1, ingredients)) |> Enum.max()
+    get_quadruples()
+    |> Enum.map(&quadruple_to_recipe/1)
+    |> Enum.map(&calc(&1, ingredients))
+    |> Enum.max_by(fn {_, p} -> p end)
+  end
+
+  def part2() do
+    ingredients = read_input()
+
+    get_quadruples()
+    |> Enum.map(&quadruple_to_recipe/1)
+    |> Enum.map(&calc(&1, ingredients))
+    |> Enum.filter(fn {r, _} -> r.calories <= 500 end)
+    |> Enum.max_by(fn {_, p} -> p end)
   end
 end
