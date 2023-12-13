2e126a243869e076ecb3918e9911e293e05b25de
Filip Bontin <fbontin@users.noreply.github.com>
2023-12-13 15:56:20 +0100

    Day 4, part 2, 2023

--- 2023/elixir/lib/day04.ex
+++ 2023/elixir/lib/day04.ex
@@ -4,7 +4,7 @@ defmodule Day4 do
   end
 
   def parse_numbers(str) do
-    str |> String.split(" ", trim: true) |> Enum.map(&(&1 |> Integer.parse() |> elem(0)))
+    str |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
   end
 
   def parse_row(str) do
@@ -26,7 +26,44 @@ defmodule Day4 do
     read_input()
     |> Enum.map(&parse_row/1)
     |> Enum.map(&get_points/1)
-    |> IO.inspect(charlists: :as_lists)
+    |> Enum.sum()
+  end
+
+  def parse_row_with_card_number(str) do
+    [card_name | [numbers | _]] = str |> String.split(": ")
+
+    card_number =
+      card_name |> String.split(~r{\s}, trim: true) |> List.last() |> String.to_integer()
+
+    [win_numbers | [your_numbers | _]] = numbers |> String.split("|")
+    {card_number, %{n: 1, w: parse_numbers(win_numbers), y: parse_numbers(your_numbers)}}
+  end
+
+  def get_new_tickets(map, i) do
+    curr = map |> Map.get(i)
+    size = MapSet.intersection(MapSet.new(curr.w), MapSet.new(curr.y)) |> MapSet.size()
+
+    if size == 0 do
+      map
+    else
+      (i + 1)..(i + size)
+      |> Enum.reduce(map, fn j, m ->
+        m |> Map.update!(j, &%{w: &1.w, y: &1.y, n: &1.n + curr.n})
+      end)
+    end
+  end
+
+  def calc(map, i, max) when i >= max, do: map
+  def calc(map, i, max), do: calc(get_new_tickets(map, i), i + 1, max)
+
+  def part2() do
+    input = read_input()
+
+    input
+    |> Enum.map(&parse_row_with_card_number/1)
+    |> Map.new()
+    |> calc(1, length(input))
+    |> Enum.map(fn {_, v} -> v.n end)
     |> Enum.sum()
   end
 end
