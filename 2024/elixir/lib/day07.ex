f49611e04163de51a3431777c40af94fe4728b98
Filip Bontin <fbontin@users.noreply.github.com>
2024-12-11 17:13:50 +0100

    Day 7, 2024 (refactored)

--- 2024/elixir/lib/day07.ex
+++ 2024/elixir/lib/day07.ex
@@ -3,44 +3,30 @@ defmodule Day7 do
   def parse_row(row), do: String.split(row, ~r":? ") |> Enum.map(&String.to_integer/1)
   def parse_input(input), do: input |> String.split("\n") |> Enum.map(&parse_row/1)
 
-  def is_possibly_true([result | values]) do
+  def is_possibly_true([result | values], ops) do
     case values do
       [value] ->
         result == value
 
       [first, second | rest] ->
-        [first * second, first + second] |> Enum.any?(&is_possibly_true([result, &1 | rest]))
+        ops
+        |> Enum.map(&apply(&1, [first, second]))
+        |> Enum.any?(&is_possibly_true([result, &1 | rest], ops))
     end
-  end
-
-  def part1() do
-    read_input()
-    |> parse_input()
-    |> Enum.filter(&is_possibly_true/1)
-    |> Enum.map(&List.first/1)
-    |> Enum.sum()
   end
 
   def concatenate(v1, v2) do
     (Integer.to_string(v1) <> Integer.to_string(v2)) |> String.to_integer()
   end
 
-  def is_possibly_true2([result | values]) do
-    case values do
-      [value] ->
-        result == value
-
-      [first, second | rest] ->
-        [first * second, first + second, concatenate(first, second)]
-        |> Enum.any?(&is_possibly_true2([result, &1 | rest]))
-    end
-  end
-
-  def part2() do
+  def sum_possibly_true_results(ops) do
     read_input()
     |> parse_input()
-    |> Enum.filter(&is_possibly_true2/1)
+    |> Enum.filter(&is_possibly_true(&1, ops))
     |> Enum.map(&List.first/1)
     |> Enum.sum()
   end
+
+  def part1(), do: sum_possibly_true_results([&Kernel.+/2, &Kernel.*/2])
+  def part2(), do: sum_possibly_true_results([&Kernel.+/2, &Kernel.*/2, &concatenate/2])
 end
